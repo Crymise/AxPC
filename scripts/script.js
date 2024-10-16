8d515ca8ea59b49039c4fcbc90f670561cb2cda8
Crymise <phedrin618@gmail.com>
2024-10-16 13:26:07 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 function sendMail (){
     let parms = {
-        name :  document.getElementById("name"),
-        communication :  document.getElementById("communication"),
-        message :  document.getElementById("message"),
+        name :  document.getElementById("name").value,
+        communication :  document.getElementById("communication").value,
+        message :  document.getElementById("message").value,
     }
     
     emailjs.send(service_lhcekd6,template_p3urris,parms).then(alert("Email Sent"))
