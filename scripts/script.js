cebec1582ab6ceb7012ab85783e5a4ead1144885
Crymise <phedrin618@gmail.com>
2024-10-16 15:57:52 +0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 function sendMail (){
     let parms = {
         name :  document.getElementById("name").value,
-        communication :  document.getElementById("communication").value,
-        message :  document.getElementById("message").value,
+        contact :  document.getElementById("contact").value,
+        algorithmname :  document.getElementById("algorithmname").value, 
+        algorithm :  document.getElementById("algorithm").value,
     }
     
     emailjs.send("service_lhcekd6","template_p3urris",parms).then(alert("Email Sent"))
